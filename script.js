ab56f613ac2d1971e563b499700971d21dbdbd00
noman abdulla <noman.fcibappy@gmail.com>
2021-08-04 11:04:42 +0600

    initial steps done 1/3

--- script.js
+++ script.js
@@ -15,6 +15,15 @@
             this.value = value;
         }
 
+        const calculateTotal = (type) => {
+            let sum = 0;
+            data.allItems[type].forEach(item => {
+                sum += item.value;
+            })
+            data.totals[type] = sum;
+            return sum;
+        }
+
         const data = {
             allItems: {
                 exp: [],
@@ -23,12 +32,13 @@
             totals: {
                 exp: 0, 
                 inc: 0
-            }
+            },
+            budget: 0,
+            percentage: -1,
         }
 
         return{
             addItem: ({type, des, val}) => {
-
                 let newItem,ID;
                 ID = data.allItems[type].length === 0 ? 0 : data.allItems[type][data.allItems[type].length - 1].id + 1;
                 type === 'exp' ? newItem = new Expence(ID, des, val) : newItem = new Income(ID, des, val);
@@ -39,9 +49,27 @@
             testing: () => {
                 console.log(data)
             },
-
             cacualteBudget: () => {
+                //Calculate sum of income and expenses
+                calculateTotal('inc');
+                calculateTotal('exp');
+                //Calculate the budget: income - expenses
+                data.budget = data.totals.inc - data.totals.exp;
+                //Calculate the percentage of income that we spent
+                if (data.totals.inc > 0){
+                    data.percentage = Math.round((data.totals.exp/data.totals.inc) * 100);
+                }else{
+                    data.percentage = -1;
+                }
 
+            },
+            getBudget: _ => {
+                return{
+                    budget: data.budget,
+                    totalInc: data.totals.inc,
+                    totalExp: data.totals.exp,
+                    percentage: data.percentage
+                }
             }
            
         }
@@ -60,7 +88,11 @@
             entryBtn: 'inputBtn',
             othersBlock: 'othersBlock',
             expensesEntryList: 'expensesEntryList',
-            incomeEntryList: 'incomeEntryList'
+            incomeEntryList: 'incomeEntryList',
+            budgetValue: 'budgetValue',
+            incomeAmmount: 'incomeAmmount',
+            expensesAmmount: 'expensesAmmount',
+            expensesPercentage: 'expensesPercentage'
             
         }
         const getbyID = (e) => document.getElementById(e);
@@ -82,7 +114,6 @@
                 //Create HTML string with placeholder text
                 if(type === 'inc'){
                     element = domSelectors.incomeEntryList
-                    console.log(obj.id,type)
                     html =  `
                         <div id="income-%id%" class="income__entry">
                             <span class="entry__title">%des%</span>
@@ -121,6 +152,16 @@
 
                 });
                 fieldsArr[0].focus();
+            },
+            displayBudget: (obj) => {
+                getbyID(domSelectors.budgetValue).textContent = obj.budget;
+                getbyID(domSelectors.incomeAmmount).textContent = obj.totalInc;
+                getbyID(domSelectors.expensesAmmount).textContent = obj.totalExp;
+                if(obj.percentage > 0){
+                    getbyID(domSelectors.expensesPercentage).textContent = obj.percentage+'%';
+                }else{
+                    getbyID(domSelectors.expensesPercentage).textContent = '---';
+                }
             }
         }
     })()
@@ -129,13 +170,15 @@
     const appController =  ( (bdgtCtrl,UICtrl) =>{
 
         const updateBudget = () => {
-
+            let budgetData;
             // Calculate the budget
+            bdgtCtrl.cacualteBudget();
 
             // return the budget
+            budgetData = bdgtCtrl.getBudget();
 
             //Display the budget in the UI
-
+            UICtrl.displayBudget(budgetData);
 
         }
 
@@ -143,21 +186,20 @@
             // 1. Get the field input data
             const {entryType, entryDesc, entryValue} = UICtrl.getInput();
 
-            if(entryDesc !== '' && isNaN(entryValue) && entryValue > 0){
+            if(entryDesc !== '' && !isNaN(entryValue) && entryValue > 0){
 
                 //2. Add the item to budget controller
                 const newItem = bdgtCtrl.addItem({type :entryType, des: entryDesc, val:entryValue});
 
                 //. Add the item to the UI 
                 UICtrl.addListItem(newItem, entryType);
-        
                 //. Calculate and update budget on the UI
                 updateBudget()
-                
-            }
 
+            }
             //. Clear Fields
             UICtrl.clearFields();
+            
 
 
         }
@@ -191,6 +233,12 @@
            init:  _ => {
                console.log('Application has started.');
                setupEventListener();
+               UICtrl.displayBudget({
+                    budget: 0,
+                    totalInc: 0,
+                    totalExp: 0,
+                    percentage: -1
+               });
            }
         }
     })(budgetController, uiController)
